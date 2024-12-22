7a935c56a1b7b3c3d526b0a0244be356a43b1441
HyungJune615 <hj615@cuuva.com>
2024-12-22 18:50:04 +0900

    feat: 몬스터 기본 정보 추가

---
+++ Assets/1_Scripts/Monster/MonsterBase.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class MonsterData
+{
+    public string Type;
+    public int TotalHp;
+    public int CurrentHp;
+    public float Speed;
+    public float Reward;
+}
+
+public class MonsterBase : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
