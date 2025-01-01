ad13a1ab0c10841e0a627739b0e8fae3fe1e4a90
HyungJune615 <hj615@cuuva.com>
2025-01-01 11:36:57 +0900

    feat: 타워 사거리 내 몬스터 감지 기능 추가

--- Assets/1_Scripts/Tower/Tower.cs
+++ Assets/1_Scripts/Tower/Tower.cs
@@ -1,12 +1,17 @@
+using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Tower : TowerBase
 {
     public CircleCollider2D AttackRangeCollider;
 
+    public Transform rotationTransform;
+    private HashSet<GameObject> monstersInRange = new HashSet<GameObject>();
+
     public void Init()
     {
-        InitTower(0, "Tower1", 20, 2.0f, 1.5f, 40f, 1.0f, 50);
+        InitTower(0, "Tower1", 20, 2.0f, 2.0f, 40f, 1.0f, 50);
     }
 
     public override void SetRange()
@@ -31,13 +36,41 @@ public class Tower : TowerBase
         Monster monster = collision.GetComponent<Monster>();
         if (monster != null)
         {
+            monstersInRange.Add(monster.gameObject);
             Debug.Log($"ID number {monster.MonsterId} Monster Entered Tower Range");
+        }
+    }
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        Monster monster = collision.GetComponent<Monster>();
+        Vector3 targetDirection = monster.transform.position - new Vector3(transform.position.x, transform.position.y, 0); ;
+        Vector3 direct = targetDirection.normalized;
+
+        if (direct != Vector3.zero)
+        {
+            rotationTransform.rotation = Quaternion.RotateTowards(rotationTransform.rotation,
+                Quaternion.LookRotation(direct, Vector3.up),
+                400f * Time.deltaTime);
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        Monster monster = collision.GetComponent<Monster>();
+        if (monster != null)
+        {
+            monstersInRange.Remove(monster.gameObject);
+            Debug.Log($"ID number {monster.MonsterId} Monster Exit Tower Range");
         }
     }
 
     public override void Start()
     {
         base.Start();
+
+        Init();
+        SetRange();
 
         base.SetLineRenderer(AttackRangeCollider);
     }
